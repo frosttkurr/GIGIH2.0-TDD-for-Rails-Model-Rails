a3b32ecada320906797d97d2e919bf6495f91561
frosttkurr <thisiscyber13@gmail.com>
2022-04-13 07:42:42 +0000

    Rename category validation for valid with name, add validation for category to make invalid without name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it 'is invalid without a name' do
+  it 'is valid with a name' do
     category = Category.new(
       name: 'dessert'
     )
 
     expect(category).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    category = Category.new(
+      name: nil
+    )
+
+    category.valid?
+
+    expect(category.errors[:name]).to include("can't be blank")
   end
 end
